6dfd9f044fd8c63ddd001c10d9001b70cf54f4dc
Lucas Yamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2021-10-11 16:11:55 -0300

    Add battle system prototype

---
+++ Assets/Scripts/Hurtbox.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hurtbox : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if this hurtbox collided with an hitbox
+        Hitbox hitbox = other.gameObject.GetComponent<Hitbox>();
+        if(hitbox)
+        {
+            // Receive damage
+            print($"{this.transform.parent.name} got {hitbox.Damage} damage from {other.transform.parent.name}");
+        }
+    }
+}
