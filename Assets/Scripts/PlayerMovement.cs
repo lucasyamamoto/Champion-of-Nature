6a14f7c87b6fd71c986199d718adafa5b1b5db98
Lucas Yamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2021-10-11 01:31:35 -0300

    Add comments and improve legibility

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rigidBody;
     [SerializeField] private float speed;
-    [SerializeField] private float zCompensation;
+    [SerializeField] private float zCompensation;   // Speed compensation on z-axis for camera angle
     [SerializeField] private float jumpForce;
 
     // Start is called before the first frame update
@@ -18,9 +18,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rigidBody.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rigidBody.velocity.y, Input.GetAxis("Vertical") * zCompensation * speed);
+        // Walking movement
+        rigidBody.velocity = new Vector3(
+            Input.GetAxis("Horizontal") * speed, 
+            rigidBody.velocity.y, 
+            Input.GetAxis("Vertical") * speed * zCompensation
+        );
+        
+        // Jumping movement
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            // Reset Y velocity and re-apply jump force
             rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0f, rigidBody.velocity.z);
             rigidBody.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
         }
