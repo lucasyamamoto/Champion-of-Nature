cc5c58dd064a64e478c560435ca2988e9dd0e4f5
Lucas Yamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2021-10-12 01:18:12 -0300

    Add preview of the battle system

--- Assets/Scripts/Hitbox.cs
+++ Assets/Scripts/Hitbox.cs
@@ -7,15 +7,31 @@ public class Hitbox : MonoBehaviour
     [SerializeField] private float damage;
     public float Damage { get => damage; set => damage = value; }
 
+    private PlayerMovement parentMovement;
+    private BoxCollider boxCollider;
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        parentMovement = GetComponentInParent<PlayerMovement>();
+        boxCollider = GetComponent<BoxCollider>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(parentMovement && (boxCollider.transform.localPosition.x >= 0 != parentMovement.FacingRight))
+        {
+            // Flip box collider
+            boxCollider.transform.localPosition = new Vector3(
+                boxCollider.transform.localPosition.x * (-1f),
+                boxCollider.transform.localPosition.y,
+                boxCollider.transform.localPosition.z
+            );
+
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,59 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public enum JumpState { OnGround, Jumping, Falling, DoubleJumping, DoubleJumpingFalling };
+
     private Rigidbody rigidBody;
     [SerializeField] private float speed;
     [SerializeField] private float zCompensation;   // Speed compensation on z-axis for camera angle
     [SerializeField] private float jumpForce;
+    [SerializeField] private float dashForce;
+    private JumpState jumpState;                    // Jump state machine to avoid infinite jumps
+    private bool facingRight;
+
+    public bool FacingRight { get => facingRight; }
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        jumpState = JumpState.OnGround;
+        facingRight = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Update facing direction
+        if(Input.GetAxis("Horizontal") != 0f)
+        {
+            facingRight = (Input.GetAxis("Horizontal") > 0f);
+        }
+
+        // Update jump state
+        if (rigidBody.velocity.y > 0f)
+        {
+            if (jumpState == JumpState.OnGround)
+            {
+                jumpState = JumpState.Jumping;
+            }
+        }
+        else if (rigidBody.velocity.y < 0f)
+        {
+            if (jumpState == JumpState.Jumping)
+            {
+                jumpState = JumpState.Falling;
+            }
+            else if (jumpState == JumpState.DoubleJumping)
+            {
+                jumpState = JumpState.DoubleJumpingFalling;
+            }
+        }
+        else if (jumpState == JumpState.Falling || jumpState == JumpState.DoubleJumpingFalling)
+        {
+            jumpState = JumpState.OnGround;
+        }
+
         // Walking movement
         rigidBody.velocity = new Vector3(
             Input.GetAxis("Horizontal") * speed, 
@@ -26,9 +65,28 @@ public class PlayerMovement : MonoBehaviour
         );
         
         // Jumping movement
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && jumpState == JumpState.OnGround)
         {
             // Reset Y velocity and re-apply jump force
+            //rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0f, rigidBody.velocity.z);
+            rigidBody.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
+        }
+
+        /* For preview purposes only! Those below may change and/or be moved to a different component */
+        // Basic attack (Eldritch Edge)
+        transform.GetChild(1).gameObject.SetActive(Input.GetKey(KeyCode.Z));
+
+        // Water skill (dash)
+        if (Input.GetKeyDown(KeyCode.X) && jumpState == JumpState.OnGround)
+        {
+            rigidBody.AddForce(new Vector3((float)System.Math.Ceiling(Input.GetAxis("Horizontal")) * dashForce, 0f, 0f), ForceMode.Impulse);
+        }
+
+        // Air skill (double jump)
+        if (Input.GetKeyDown(KeyCode.C) && (jumpState == JumpState.Jumping || jumpState == JumpState.Falling))
+        {
+            // Reset Y velocity and re-apply jump force
+            jumpState = JumpState.DoubleJumping;
             rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0f, rigidBody.velocity.z);
             rigidBody.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
         }
