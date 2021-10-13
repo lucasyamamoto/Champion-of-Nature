f4a599b6d715913c0b17da668fc4cc297820e14b
Lucas Yamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2021-10-13 11:24:54 -0300

    Add health system

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private float health;
+
+    public float Health
+    {
+        get => health;
+        set
+        {
+            health = value;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Hurtbox.cs
+++ Assets/Scripts/Hurtbox.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Hurtbox : MonoBehaviour
 {
-
+    private Character character;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        character = GetComponentInParent<Character>();
     }
 
     // Update is called once per frame
@@ -22,10 +22,11 @@ public class Hurtbox : MonoBehaviour
     {
         // Check if this hurtbox collided with an hitbox
         Hitbox hitbox = other.gameObject.GetComponent<Hitbox>();
-        if(hitbox)
+        if(hitbox && character)
         {
             // Receive damage
             print($"{this.transform.parent.name} got {hitbox.Damage} damage from {other.transform.parent.name}");
+            character.Health -= hitbox.Damage;
         }
     }
 }
