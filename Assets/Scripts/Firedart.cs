576fa884c2e694f9f2abc4285f20ff3f2844358f
Lucas Yamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2021-11-21 02:14:51 -0300

    Fix firedart

--- Assets/Scripts/Firedart.cs
+++ Assets/Scripts/Firedart.cs
@@ -8,7 +8,11 @@ public class Firedart : CharacterSkill
     [SerializeField] private float delay;
     [SerializeField] private GameObject projectile;
     private Animator animator;
+    private CharacterMovement characterMovement;
+    private GameObject projectileLeft;
+    private GameObject newProjectile;
     private bool isReady;
+    public bool releaseProjectile;
 
     private InputManager.KeyStatus currentInput;
 
@@ -17,15 +21,14 @@ public class Firedart : CharacterSkill
     IEnumerator Shoot()
     {
         isReady = false;
+        characterMovement.Block = true;
+
+        // Generate projectile
+        newProjectile = Instantiate((transform.rotation.y == 0f) ? projectile : projectileLeft, projectile.transform.position, projectile.transform.rotation);
+        newProjectile.SetActive(false);
+
+        // Start animation
         animator.SetTrigger("Firedart");
-
-        float direction = ((transform.rotation.y == 0f) ? 1f : -1f);
-
-        Vector3 newProjectilePos = transform.position + projectile.transform.localPosition * transform.localScale.x * direction;
-
-        GameObject newProjectile = Instantiate(projectile, newProjectilePos, projectile.transform.rotation);
-        newProjectile.GetComponent<Projectile>().Speed *= direction;
-        newProjectile.SetActive(true);
 
         yield return new WaitForSecondsRealtime(delay);
 
@@ -34,12 +37,35 @@ public class Firedart : CharacterSkill
         yield return null;
     }
 
+    void ReleaseProjectile()
+    {
+        // Update projectile direction
+        float direction = ((transform.rotation.y == 0f) ? 1f : -1f);
+        newProjectile.transform.position = transform.position + projectile.transform.localPosition * transform.localScale.x * direction;
+
+        // Activate projectile
+        newProjectile.SetActive(true);
+
+        // Reset state
+        newProjectile = null;
+        characterMovement.Block = false;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize member variables
         animator = GetComponent<Animator>();
+        characterMovement = GetComponent<CharacterMovement>();
         isReady = true;
         currentInput = new InputManager.KeyStatus();
+        newProjectile = null;
+        releaseProjectile = false;
+
+        // Generate left version of projectile
+        projectileLeft = Instantiate(projectile);
+        projectileLeft.SetActive(false);
+        projectileLeft.GetComponent<Projectile>().Speed *= -1f;
     }
 
     // Update is called once per frame
@@ -48,6 +74,11 @@ public class Firedart : CharacterSkill
         if (currentInput.keyDown && isReady)
         {
             StartCoroutine(Shoot());
+        }
+
+        if (newProjectile && releaseProjectile)
+        {
+            ReleaseProjectile();
         }
     }
 }
