c582a43eaa3cc904bbf54f2d0d2168c69e23d5e5
Lucas Yamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2021-11-23 08:51:47 -0300

    Add water ranged enemy

--- Assets/Scripts/Stoneshield.cs
+++ Assets/Scripts/Stoneshield.cs
@@ -23,7 +23,7 @@ public class Stoneshield : CharacterSkill
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         shield.gameObject.SetActive(currentInput.key);
         characterMovement.Block = currentInput.key;
