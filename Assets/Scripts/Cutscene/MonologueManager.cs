387b54b04fdde83a9e3fb152c7791d50f9f1032e
Diogo Castanho Emidio <diogo.ce@usp.br>
2021-12-02 22:17:43 -0300

    Cutscene finished (without song)

--- Assets/Scripts/Cutscene/MonologueManager.cs
+++ Assets/Scripts/Cutscene/MonologueManager.cs
@@ -8,17 +8,22 @@ public class MonologueManager : MonoBehaviour
 {
     [SerializeField] Text speechBalloon;
     [SerializeField] List<GameObject> images;
-    private bool corroutine;
+    [SerializeField] Button nextButton;
+    [SerializeField] GameObject skipButton;
+    private Coroutine coroutine;
+    private bool writing;
     private int indexMonologue, indexImage, i;
     private string[] monologue = {
-        "Hello, there! My name is Mikhail and now I'm going to tell you a story, so get really comfortable and pay attention!",
-        "Every year, the Council of Mages opens its doors to welcome new apprentices. As many want this opportunity, eventually this selection process became corrupted and skewed.",
-        "Also, as each school of magic has a seat limit, people with a lot of potential, but with a less privileged origin, ended up never learning magic.",
-        "So, over time, the quality of mages and their reputation declined. To combat this decay, the Council decided to create a selective process: the Champion of Nature!",
-        "Taking place every three years, its beginning is simple: every neophyte learns Eldritch Edge, a basic melee combat spell that conjures a blade made of pure arcane energy.",
-        "Afterwards, participants are loaned the powers of two of the four elements through enchanted bracelets with spells from the chosen schools.",
-        "The powers are: Aquaslide (water dash), Stoneshield (shield made of, well, stones), Firedart (fireball attack - innovative, isn't it?) and Windstep (double jump).",
-        "Finally, the competitors are thrown into The Tower: a dungeon full of enemies controlled by the Council. Don't be afraid to kill them, because they are not real people... I guess."
+        "Hello, there! My name is Mikhail and now I'm going to tell you a story, so get really comfortable and pay attention because this tutorial won't be shown again!",
+        "Every year, the Council of Mages opens its doors to welcome new apprentices (that's you). As many want this opportunity, slowly the selection process became corrupted and skewed...",
+        "Also, as each school of magic has a seat limit, people with a lot of potential, but with a less privileged origin, ended up never learning magic (I hope that's not you)...",
+        "So, over time, the quality of mages and their reputation declined. To combat this decay, the Council decided to create a selective process: the Champion of Nature (aka game's name)...",
+        "Taking place every three years, its beginning is simple: every neophyte (again, you) learns Eldritch Edge, a melee combat spell that conjures a blade made of pure arcane energy...",
+        "Afterwards, participants are loaned the powers of two of the four elements (guess which ones) through enchanted bracelets with spells from the chosen schools...",
+        "The powers are: Aquaslide (water dash, weeeh), Stoneshield (shield made of, well, stones), Firedart (fireball attack - innovative, eh?) and Windstep (double jump - suck it, Physics)...",
+        "Finally, the competitors are thrown into The Tower: a dungeon full of enemies controlled by the Council. Don't be afraid to kill them, because they are not real people... I guess...",
+        "Whoever manages to reach the end is admitted. Easy, right? To survive, you'll have to use your head (and your hands. In fact, mostly your hands), so memorize this scroll:",
+        "That's all, Folks! Good luck, young apprentice (I could say Padawan, but copyrights forbid me)!"
     };
 
     // Skip to main menu
@@ -33,25 +38,24 @@ public class MonologueManager : MonoBehaviour
     {
         if (indexMonologue < monologue.Length)
         {
-            if (!corroutine)
+            if (!writing)
             {
-                if (indexMonologue >= 5 && indexMonologue <= 6)
+                if (indexMonologue >= 5 && indexMonologue <= 9)
                     images[indexImage++].SetActive(false);
 
-                corroutine = true;
-                StartCoroutine(wait(.03f));
+                writing = true;
+                coroutine = StartCoroutine(wait(.03f));
             }
             else
             {
-                StopAllCoroutines();
+                StopCoroutine(coroutine);
                 speechBalloon.text = monologue[indexMonologue];
 
-                if (indexMonologue >= 4 && indexMonologue <= 5)
-                    images[indexImage].SetActive(true);
-                indexMonologue++;
-                corroutine = false;
+                printImage();
             }
         }
+        else
+            skip();
     }
 
     // Write letter by letter
@@ -63,16 +67,29 @@ public class MonologueManager : MonoBehaviour
             yield return new WaitForSeconds(waitTime);
         }
 
-        if (indexMonologue >= 4 && indexMonologue <= 5)
+        printImage();
+    }
+
+    private void printImage()
+    {
+        if (indexMonologue >= 4 && indexMonologue <= 8)
+        {
             images[indexImage].SetActive(true);
-        indexMonologue++;
-        corroutine = false;
+            indexMonologue++;
+        }
+        else if (++indexMonologue == monologue.Length)
+        {
+            skipButton.SetActive(false);
+            nextButton.GetComponentInChildren<Text>().text = "<b>CONTINUE</b>";
+        }
+
+        writing = false;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        corroutine = false;
+        writing = false;
         indexMonologue = indexImage = 0;
     }
 
